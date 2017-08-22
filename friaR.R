a47e33eee759e4fa084adc7e2a9a8b7fd9171773
Stuart Illson <sillson88@gmail.com>
2017-08-22 16:25:04 -0700

    fixed

--- friaR.R
+++ friaR.R
@@ -2,15 +2,17 @@
 ## friaR ##
 #################################
 
-sauce = "~/personal_projects/friaR/dependencies/env_scripts.R"
 ##########
 ## Set ENV
 ##########
-source(sauce,local=TRUE)
+
+directory <- list.dirs(path="~/personal_projects/friaR/dependencies/", full.names = T)
+files = list.files(directory)
+for ( file in files ) { source(paste0(directory, file), local=TRUE)}
 loadEnv("friaR")
+
 
 ############
 ## Clean ENV
 ############
-source(sauce,local=TRUE)
 cleanEnv()
