ae3bf26954d3f4df46b1ba60032ea145a9cc0715
Stuart Illson <sillson88@gmail.com>
2017-08-23 10:16:24 -0700

    dickey-fuller

--- friaR.R
+++ friaR.R
@@ -12,19 +12,31 @@ for ( file in files ) { source(paste0(directory, file), local=TRUE)}
 
 loadEnv("friaR")
 
+# Doing a lot of evaluating strings as variables
 eval_as_var <- function(str) {
   eval(parse(text=str))
 }
 
+# Yesterday's date
+yesterday <- format(Sys.Date()-1, "%Y-%m-%d")
+
 for ( stock in stock_list ) {
-  # Get stock data from feature list
-  getSymbols(stock, from='2015-01-01', to='2017-08-21')
+  # Get stock data from feature list from start of 2015 to yesterday
+  getSymbols(stock, from='2015-01-01', to=yesterday)
   # We only care about the close price series
   assign(stock, eval_as_var(stock)[,4])
   # Compute logarithmic returns of the stock for the ARIMA model.
   # We want to forecast log returns and not stock prices
   assign(stock, diff(log(eval_as_var(stock)),lag=1))
   assign(stock, eval_as_var(stock)[!is.na(eval_as_var(stock))])
+  # Evaluate stock and remove if non-stationary
+  print(paste("Evaluating stock with Augmented Dickey-Fuller Test -- ", stock))
+  evaluation <- adf.test(eval_as_var(stock))
+  
+  # Remove stock from the list if non-stationary
+  if (evaluation[3] != "stationary") {
+    stock_list <<- stock_list[stock_list != stock]
+  }
 }
 
 
